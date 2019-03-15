8378597458f3d899fc08c1fe46c1bfbe7ce7e03a
Jacek Boguta <jacekboguta@gmail.com>
2019-03-15 09:47:59 +0100

    layout

--- app/src/main/java/com/example/cyklyciaaktywnoci/Activity0.java
+++ app/src/main/java/com/example/cyklyciaaktywnoci/Activity0.java
@@ -2,12 +2,56 @@ package com.example.cyklyciaaktywnoci;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Activity0 extends AppCompatActivity {
+    private EditText poleTekstowe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity0);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+
+    public void A0Button0Click(View view) {
+    }
+
+    public void A0Button1Click(View view) {
+    }
+
+    public void A0Button2Click(View view) {
     }
 }
